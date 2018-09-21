e7f8769325641be17df5e09a2e9d5f3fee7d73c1
abhik <abhik.ghosh5@gmail.com>
2018-09-21 20:21:50 +0530

    first commit

---
+++ Digit Recognition/SVM_DigitRecognizer.R
@@ -0,0 +1,17 @@
+############################ SVM Digit Recogniser #################################
+# 1. Business Understanding
+# 2. Data Understanding
+# 3. Data Preparation
+# 4. Model Building 
+#  4.1 Linear kernel
+#  4.2 RBF Kernel
+# 5 Hyperparameter tuning and cross validation
+
+#####################################################################################
+
+# 1. Business Understanding: 
+
+#develop a model using Support Vector Machine which should correctly classify the 
+#handwritten digits based on the pixel values given as features.
+
+#####################################################################################
